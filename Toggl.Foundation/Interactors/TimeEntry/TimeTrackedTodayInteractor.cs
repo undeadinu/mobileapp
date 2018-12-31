c02bb1fd7371d3a67a1ef048bd7402c346aad138
Simon Rozsival <simon@rozsival.com>
2018-12-31 15:31:11 +0100

    :sparkles: Add interactor

---
+++ Toggl.Foundation/Interactors/TimeEntry/TimeTrackedTodayInteractor.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Reactive;
+using System.Reactive.Linq;
+using Toggl.Foundation.DataSources;
+using Toggl.Foundation.Extensions;
+using Toggl.Multivac;
+using Toggl.Multivac.Extensions;
+
+namespace Toggl.Foundation.Interactors
+{
+    public sealed class TimeTrackedTodayInteractor : IInteractor<IObservable<TimeSpan>>
+    {
+        private readonly ITimeService timeService;
+        private readonly ITimeEntriesSource timeEntries;
+
+        public TimeTrackedTodayInteractor(
+            ITimeService timeService,
+            ITimeEntriesSource timeEntries)
+        {
+            Ensure.Argument.IsNotNull(timeService, nameof(timeService));
+            Ensure.Argument.IsNotNull(timeEntries, nameof(timeEntries));
+
+            this.timeService = timeService;
+            this.timeEntries = timeEntries;
+        }
+
+        public IObservable<TimeSpan> Execute()
+            => timeEntries.ItemsChanged()
+                .StartWith(Unit.Default)
+                .SelectMany(_ => calculateTimeTrackedToday());
+
+        private IObservable<TimeSpan> calculateTimeTrackedToday()
+            => timeEntries.GetAll(timeEntry =>
+                    timeEntry.Start.LocalDateTime.Date == timeService.CurrentDateTime.LocalDateTime.Date
+                    && timeEntry.Duration != null)
+                .SingleAsync()
+                .SelectMany(CommonFunctions.Identity)
+                .Sum(timeEntry => timeEntry.Duration ?? 0.0)
+                .Select(TimeSpan.FromSeconds);
+    }
+}
